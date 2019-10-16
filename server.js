6818d91297560473af238965cf9548956eb0b141
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2019-10-16 16:34:55 -0700

    finished

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express ();
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 const Veggies = require('./Models/veggies.js');
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
 
 app.get('/veggies', (req,res)=>{
     res.render('index.ejs',{
         veggies: Veggies
     });
 });
+
+app.get('/veggies/:index', (req, res) => {
+    res.render('show.ejs',{
+        veg: Veggies[req.params.index]
+    })
+})
+
+app.delete('/veggies/:index', (req, res) => {
+    Veggies.splice(req.params.index, 1);
+    res.redirect('/veggies');
+})
 
 app.listen(3000, () => {
     console.log("app is running on port:",
